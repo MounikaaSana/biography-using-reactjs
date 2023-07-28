ad028b1d968c526a705e79b5660f7d10153fc7dc
MounikaaSana <104676932+MounikaaSana@users.noreply.github.com>
2023-07-28 21:51:40 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,171 @@
+ReactDOM.render(   
+<div>
+<center>
+    <h1><b>vijay thalapathy</b></h1>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPYQq6ygB5TIaVRT0KVJXFb6_DILf6EZtnYw&usqp=CAU" />
+    <p>Joseph Vijay Chandrasekhar (born 22 June 1974), known professionally as Vijay, is an Indian actor and singer who works predominantly in Tamil cinema. He is among the highest paid actors in India and has featured in Forbes India's Celebrity 100 list on seven occasions.He has played the lead in 66 films and the International Business Times framed him as a "consistent performer".Referred to as Thalapathy (transl. Commander), Vijay has a significant following internationally.He has won several awards, including an Osaka Best Actor Award and a South Indian International Movie Award.In 2023, he has become the most highest paid actor in India and one of the top paid actor in the world.</p>
+    <h2>Early life and family</h2>
+    <p>Vijay was born as Joseph Vijay Chandrasekhar on 22 June 1974 in Madras (now Chennai), Tamil Nadu.His father S. A. Chandrasekhar is a Tamil film director and his mother Shoba Chandrasekhar is a playback singer and carnatic vocalist. His father is of Christian Vellalar descent and his mother is Hindu.Vijay was baptised at the age of 12.Vijay had a sister, Vidhya, who died when she was just two years old Initially attending Fathima Matriculation Higher Secondary School at Kodambakkam, Vijay later joined the Balalok Matriculation Higher Secondary School at Virugambakkam and went on to pursue a degree in Visual Communications from Loyola College.</p>
+    <h1><u>film career</u></h1>
+    <h3>1984–2003: Child actor and transition into lead roles</h3>  
+    <p>At the age of 10, Vijay started his film career as a child actor in Vetri (1984), making his first paycheck of ₹500 rupees paid for by actor-and-producer P. S. Veerappa. He then performed as a child actor in films such as Kudumbam (1984), Vasantha Raagam (1986), Sattam Oru Vilayaattu (1987) and Ithu Engal Neethi (1988). He also performed in Naan Sigappu Manithan (1985) as a co-star to Rajinikanth, who was the lead actor.Vijay started to play lead roles at the age of 18 starting with Naalaiya Theerpu (1992).Vijay then appeared in the films Sendhoorapandi, Rasigan, Deva and Coimbatore Mappillai. They were commercially successful, though not critically acclaimed.</p><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/98vjactor.jpg/260px-98vjactor.jpg"/>
+    <h3>2003–2011: Star status in Tamil cinema</h3>
+    <p> Ghilli was the first Tamil film of all time to gross over ₹500 million, or ₹50 crore, in the domestic box office. The film grossed nearly $500,000 in the Malaysian market.The Hindu stated that "Vijay, the hero whom the masses today identify with, and Prakash Raj, the inimitable villain in tow, this flick, "Ghilli"...on a winning track".Vijay at a function in 2007 Ghilli was followed by commercially successful films Madhurey and Thirupaachi in 2005.Later, Vijay starred in the commercially and critically successful films Sachein, Sivakasi and Pokkiri. Pokkiri was one of the highest-grossing films in his career at that time.all three films were more successful overseas.In 2009, Vijay became one of the highest paid actors in South India.In 2010, he acted in the action comedy film Sura. Sura was Vijay's 50th film as a lead actor.</p>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Chinesefestival2011Vijay.jpg/279px-Chinesefestival2011Vijay.jpg"/>
+    <h3>2012–present: Increased critical and commercial success globally</h3>
+    <p>The action-thriller Theri directed by Atlee was released in April 2016 to mixed reviews from critics and audiences, with one reviewer criticizing the actor's performance and the "predictable" storyline.It was the second-highest-grossing Tamil film of 2016 and Vijay's second film to gross over ₹1,710,000,000.In 2017, the masala film Bairavaa was released. It received mixed reviews and it collected ₹115 crore at the box office.His 61st film, the action thriller Mersal, was directed by Atlee and released Diwali of 2017. It was both a critical and commercial success,becoming the first of Vijay's films to gross over ₹2.5 billion.The film was also released in Japan in the cities of Tokyo, Ebina, Osaka and Nakaya.For his role in Mersal, Vijay was nominated for Best Actor at the National Film Awards UK in 2018. The film won the Best Foreign Language Film award at the same event.</p>
+ 
+ <h3>Upcoming Movie</h3>
+ <h4>Leo<b>--get ready for the leo</b></h4>
+
+<h3>Movies released in 2023</h3>
+<h6>Vaarasudu</h6>
+<h6>Varisu (Hindi)</h6>
+<h6>Varisu</h6>
+
+<h3>Movies released in 2022</h3>
+<h6>Beast</h6>
+<h6>RAW (Beast)</h6>
+
+<h3>Movies released in 2021</h3>
+<h6>Vijay The Master</h6>
+<h6>Master</h6>
+
+<h3>Movies released in 2019</h3>
+<h6>Bigil</h6>
+<h6>Whistle</h6>
+
+<h3> Movies released in 2018</h3>
+<h6>Sarkar</h6>
+<h6>Bhairava (Hindi)</h6>
+
+<h3> Movies released in 2017</h3>
+<h6>Adirindi</h6>
+<h6>Mersal</h6>
+<h6>Pokkiri Simon</h6>
+<h6>Sura</h6>
+<h6>Agent Bhairava</h6>
+<h6>Ek Aur Jaanbaaz Khiladi</h6>
+<h6>Bairavaa</h6>
+
+<h3> Movies released in 2016</h3>
+<h6>Mass Raja</h6>
+<h6>Wanted Baghi</h6>
+<h6>Police</h6>
+<h6>Policeodu</h6>
+<h6>Theri</h6>
+
+<h3>Movies released in 2015</h3>
+<h6>Puli</h6>
+<h6>Actor</h6>
+<h6>Jilla (Telugu)</h6>
+<h6>Kaavalan</h6>
+
+<h3> Movies released in 2014</h3>
+<h6>Kaththi</h6>
+<h6>Jilla (Tamil)</h6>
+
+<h3>Movies released in 2013</h3>
+<h6>Anna (Telugu)</h6>
+<h6>Thalaivaa</h6>
+<h6>Dammunte Kasko</h6>
+<h6>Main Hoon Bodyguard</h6>
+
+<h3>Movies released in 2012</h3>
+<h6>Thuppakki</h6>
+<h6>Rowdy Rathore</h6>
+<h6>Nanban</h6>
+
+<h3> Movies released in 2011</h3>
+<h6>Velayudham</h6>
+<h6>Yama Kantri</h6>
+
+<h3>Movies released in 2010</h3>
+<h6>Sura</h6>
+
+<h3> Movies released in 2009</h3>
+<h6>Vettaikaran</h6>
+<h6>Gilli (Kannada)</h6>
+<h6>Ghamandee</h6>
+<h6>Villu</h6>
+
+<h3>Movies released in 2008</h3>
+<h6>Pandhayam</h6>
+<h6>Poi Solla Porom (Tamil)</h6>
+<h6>Kuruvi</h6>
+<h6>Sachein</h6>
+<h6>Ghilli</h6>
+
+<h3>Movies released in 2007</h3>
+<h6>Azhagiya Tamil Magan</h6>
+<h6>Sabse Bada Khiladi (2007)</h6>
+<h6>Pokkiri</h6>
+
+<h3>Movies released in 2006</h3>
+<h6>Aathi</h6>
+
+<h3>Movies released in 2005</h3>
+<h6>Sivakasi</h6>
+<h6>Sukran</h6>
+<h6>Thirupachi</h6>
+
+<h3>Movies released in 2004</h3>
+<h6>Madurey</h6>
+
+<h3>Movies released in 2003</h3>
+<h6>Thirumalai</h6>
+<h6>Ek Aur Tiger</h6>
+<h6>Pudhiya Geethai</h6>
+<h6>Puthiya Geethai</h6>
+<h6>Vaseegara</h6>
+
+<h3>Movies released in 2002</h3>
+<h6>Bagavathi</h6>
+<h6>Youth</h6>
+<h6>Thamizhan</h6>
+
+<h3>Movies released in 2001</h3>
+<h6>Shahjahan</h6>
+<h6>Badri</h6>
+<h6>Friends</h6>
+
+<h3>Movies released in 2000</h3>
+<h6>Priyamanavale</h6>
+<h6>Kushi</h6>
+<h6>Kannukkul Nilavu</h6>
+<h6>Priyamaanavale</h6>
+
+<h3>Movies released in 1999</h3>
+<h6>Minsara Kanna</h6>
+<h6>Nenjinile</h6>
+<h6>Endrendrum Kadhal</h6>
+<h6>Thulladha Manamum Thullum</h6>
+<h6>Manamum Thullum</h6>
+<h6>Dharma The Warrior</h6>
+
+<h3>Movies released in 1998</h3>
+<h6>Nilave Vaa</h6>
+<h6>Nilaave Vaa</h6>
+<h6>Priyamudan</h6>
+<h6>Ninaithen Vandhai</h6>
+
+<h3>Movies released in 1997</h3>
+<h6>Nerukku Ner</h6>
+<h6>Once More</h6>
+<h6>Love Today</h6>
+<h6>Kadhalukku Mariyadhai</h6>
+
+<h3>Movies released in 1996</h3>
+<h6>Poove Unakkagha</h6>
+<h6>Poove Unakkaga</h6>
+<h6>Coimbatore Mapilai</h6>
+<h6>Coimbatore Mappillai</h6>
+
+<h3>Movies released in 1995</h3>
+<h6>Rajavin Parvaiyile</h6>
+
+   to know more <a href="https://www.google.com/search?q=vijay+thalapathy+wikipedia&rlz=1C1CHZN_enIN1062IN1062&oq=vijay+&aqs=chrome.1.69i59l3j69i57j69i61j69i60.378337468j0j15&sourceid=chrome&ie=UTF-8">Click Here</a>  
+
+   </center> 
+</div>,
+document.getElementById("content"));
